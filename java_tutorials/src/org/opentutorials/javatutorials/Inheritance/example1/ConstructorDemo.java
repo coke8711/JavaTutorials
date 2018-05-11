7ec8a057f260f1062b8fd21129c58fc90c830915
TWRT <TWRT@192.168.1.45>
2018-05-11 17:31:52 +0900

    0511열심히

---
+++ java_tutorials/src/org/opentutorials/javatutorials/Inheritance/example1/ConstructorDemo.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.Inheritance.example1;
+
+public class ConstructorDemo {
+	public ConstructorDemo() {}
+	public ConstructorDemo(int param1) {}
+	public static void main(String[] args) {
+		ConstructorDemo c = new ConstructorDemo();
+	}
+
+}
